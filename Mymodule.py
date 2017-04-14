240958b6b18d7ed71e554680b2102e8a02aab0dc
ehuynen <ehuynen@deloitte.com>
2017-04-14 11:33:00 +0200

    Animals gecreerd + informatie over het importeren van functies in bibliotheken

--- Mymodule.py
+++ Mymodule.py
@@ -3,8 +3,6 @@ class Animal (object):
         self.name = startname
         self.age = age
     def description(self):
-        print("this is "+ self.name)
+        print("This is "+ self.name)
         print("He/she is " + str(self.age)+" years old.")
 
-lion = Animal("Harry", 11)
-print(lion.description)
